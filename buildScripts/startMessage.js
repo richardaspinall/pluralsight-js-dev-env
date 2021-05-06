d0413667a1e69cd4a68d8b8c41d9848618ea1253
Richard Aspinall <rls.aspinall@gmail.com>
2021-05-06 12:21:21 +1000

    Added files

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,5 @@
+// const chalk = require('chalk');
+
+// We are using ES6 here and going to make babel transpile it to the above
+import chalk from 'chalk';
+console.log(chalk.green('Starting app in dev mode...'));
