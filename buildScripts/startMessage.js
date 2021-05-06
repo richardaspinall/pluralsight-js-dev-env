30e13b4a6272810c4c6c5b01c24a85368adefff5
Richard Aspinall <rls.aspinall@gmail.com>
2021-05-06 16:53:44 +1000

    Added linting and bundling

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -2,4 +2,4 @@
 
 // We are using ES6 here and going to make babel transpile it to the above
 import chalk from 'chalk';
-console.log(chalk.green('Starting app in dev mode...'));
+console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
